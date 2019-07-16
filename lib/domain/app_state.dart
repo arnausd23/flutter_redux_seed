5e9b9953cbff71a59857870e97b1f7fd86686a78
Albert Parrón <al.parron@gmail.com>
2019-07-16 12:34:13 +0200

    Corrected bug

--- lib/domain/app_state.dart
+++ lib/domain/app_state.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter_redux_seed/model/item.dart';
+import 'package:flutter_redux_seed/domain/item.dart';
 
 class AppState {
   final List<Item> items;
